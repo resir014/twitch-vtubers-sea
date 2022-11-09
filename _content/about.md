1492891074442d801423ed2e05c9a2ce396f04c6
Resi Respati <resir014@gmail.com>
2022-11-09 12:28:16 +0700

    feat: update about page + banner

---
+++ _content/about.md
@@ -0,0 +1,25 @@
+---
+title: About
+description: 'Database of Twitch VTubers from South East Asia.'
+---
+
+While the rise of VTubers over the past few years — especially on the Southeast Asian
+region — is incredible to see, we felt like there&apos;s not much love for VTubers who
+primarily stream on [Twitch](https://www.twitch.tv/).
+
+Therefore, we sought to create this open and actively-updated database on well-known
+VTubers who stream on this platform we all know and love!
+
+## Contributing to the database
+
+We're currently setting up a submission/removal process for the database. Watch this space for more updates!
+
+In the meantime, you can mention/DM either [@resir014](https://twitter.com/resir014) or [@KATSUKi_D_TUBE](https://twitter.com/KATSUKi_D_TUBE) on Twitter if you want to be added to the database.
+
+## Maintainers
+
+The list is maintained by the following people. If you want to be added into this
+list, please mention/DM one of them!
+
+- [@resir014](https://twitter.com/resir014)
+- [@KATSUKi_D_TUBE](https://twitter.com/KATSUKi_D_TUBE)
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,18 +1,34 @@
 import * as React from 'react';
+import htmr from 'htmr';
+import type { InferGetStaticPropsType } from 'next';
 import { NextSeo } from 'next-seo';
 import { BaseLayout } from '~/components/layouts/base-layout';
-import { Container } from '~/components/ui/container';
-import siteConfig from '~/modules/content/site-config';
-import { PageHeader } from '~/components/page/page-header';
 import { Page } from '~/components/page/page';
+import { PageHeader } from '~/components/page/page-header';
+import { Container } from '~/components/ui/container';
+import { getAboutPage } from '~/modules/content/about';
+import siteConfig from '~/modules/content/site-config';
 
-const meta = {
-  title: 'About',
-  description: siteConfig.site_description,
-  tagline: siteConfig.site_tagline,
-};
+export async function getStaticProps() {
+  const { data, content } = await getAboutPage();
 
-export default function AboutPage() {
+  return {
+    props: {
+      data,
+      content,
+    },
+  };
+}
+
+type AboutPageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function AboutPage({ data, content }: AboutPageProps) {
+  const meta = {
+    title: typeof data.title === 'string' ? data.title : String(data.title),
+    description: typeof data.description === 'string' ? data.description : String(data.description),
+    tagline: siteConfig.site_tagline,
+  };
+
   return (
     <BaseLayout>
       <NextSeo
@@ -24,53 +40,7 @@ export default function AboutPage() {
         <Container>
           <PageHeader pageTitle={meta.title} description={meta.tagline} />
           <div className="prose prose-md lg:prose-lg prose-indigo mx-auto mt-8 lg:mt-12">
-            <p>
-              While the rise of VTubers over the past few years — especially on the Southeast Asian
-              region — is incredible to see, we felt like there&apos;s not much love for VTubers who
-              primarily stream on <a href="https://www.twitch.tv/">Twitch</a>.
-            </p>
-            <p>
-              Therefore, we sought to create this open and actively-updated database on well-known
-              VTubers who stream on this platform we all know and love!
-            </p>
-
-            <h2>Maintainers</h2>
-            <p>
-              The list is maintained by the following people. If you want to be added into this
-              list, please mention/DM one of them!
-            </p>
-            <ul>
-              <li>
-                <a href="https://twitter.com/resir014" target="_blank" rel="noopener noreferrer">
-                  @resir014
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://twitter.com/KATSUKi_D_TUBE"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  @KATSUKi_D_TUBE
-                </a>
-              </li>
-            </ul>
-
-            <h2>Notice</h2>
-            <p>
-              This project is a <strong>proof of concept</strong>, and still uses sample data of the
-              VTubers that I know. Once the data structure is finalised, and a submission/removal
-              process is laid out, I&apos;ll be opening this list out and will replace the
-              &quot;database&quot; with actual data.
-            </p>
-            <p>
-              Since I&apos;m personally not that deeply-invested into the VTuber scene, I will need
-              help in updating this database. Let me know at{' '}
-              <a href="https://twitter.com/resir014" target="_blank" rel="noopener noreferrer">
-                @resir014
-              </a>{' '}
-              on Twitter if you&apos;re interested.
-            </p>
+            {htmr(content)}
           </div>
         </Container>
       </Page>
